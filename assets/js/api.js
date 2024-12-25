be641c86595246b7fcb7d818ff2596f508546c68
lumens7 <142345222+lumens7@users.noreply.github.com>
2024-12-25 12:15:12 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData(){
-    const url = ''
+    const url = 'https://raw.githubusercontent.com/lumens7/hortifruti/refs/heads/main/json/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }
